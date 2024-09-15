7e068b6124f39955ca21eb45940a3f0d84cb5f7a
R3D1NM <r3dinm@gmail.com>
2024-09-15 04:53:52 +0000

    [feat] re-construct main page & basketList page & example basket (ONE-633)

--- src/pages/Main/components/BasketCard.tsx
+++ src/pages/Main/components/BasketCard.tsx
@@ -1,31 +1,33 @@
 import {Link, useNavigate} from 'react-router-dom';
-
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from '@/components/ui/card';
+import {Button} from '@/components/ui/button';
+import {cn} from '@/lib/utils';
 
 const BasketCard = ({text, login}: {text: string; login: boolean}) => {
     return (
         <>
-            <Card className="w-full bg-gradient-to-br from-oneit-blue to-[#a3dbff] hover:from-[#a3dbff] hover:to-[#98d5fb] transition-colors duration-300 rounded-lg h-[50%] flex flex-col justify-between shadow-md border-0 max-w-md">
+            <div className="w-full bg-gradient-to-br from-oneit-blue to-[#a3dbff] hover:from-[#a3dbff] hover:to-[#98d5fb] transition-colors duration-300 rounded-lg h-fit flex flex-col justify-between shadow-md border-0 max-w-md">
                 <Link
-                    to={login ? '/basket/create' : '/login?redirect=/'}
+                    to={login ? '/basket' : '/login?redirect=/basket'}
                     className="w-full h-full flex flex-col justify-between"
                 >
-                    <CardHeader className="p-3">
-                        <CardTitle>이 중에 뭐 주지?</CardTitle>
-                        <CardDescription className="text-oneit-gray">
+                    <div className="mx-3 mt-2">
+                        <h3 className="text-2xl font-bold">이 중에 뭐 주지?</h3>
+                        <p className="text-oneit-gray text-sm">
                             뭘 좋아할지 몰라서 다 준비해봤어
-                        </CardDescription>
-                    </CardHeader>
-                    <CardContent>{text}</CardContent>
+                        </p>
+                    </div>
+                    <Button
+                        className={cn(
+                            'relative m-2',
+                            login
+                                ? ' bg-white hover:bg-slate-50 text-md'
+                                : 'bg-kakao-yellow hover:bg-kakao-yellow/90 text-oneit-gray text-md',
+                        )}
+                    >
+                        {text}
+                    </Button>
                 </Link>
-            </Card>
+            </div>
         </>
     );
 };
--- src/pages/Recommend/Results.tsx
+++ src/pages/Recommend/Results.tsx
@@ -14,13 +14,12 @@ import React, {Suspense, useEffect, useState} from 'react';
 import {
     Dialog,
     DialogContent,
+    DialogDescription,
+    DialogFooter,
     DialogHeader,
     DialogTitle,
     DialogTrigger,
 } from '@/components/ui/dialog';
-import Kakao from '@/assets/kakao.png';
-import Naver from '@/assets/naver_blog.png';
-import Instagram from '@/assets/instagram.png';
 import {Spinner} from '@/components/ui/spinner';
 import {getGift} from '@/api/product';
 import {Card, CardTitle} from '@/components/ui/card';
@@ -34,12 +33,17 @@ import {
 import Share from '@/components/common/Share';
 import KakaoShare from '@/components/common/KakaoShare';
 import {isLoginAtom} from '@/api/auth';
+import {toast} from 'sonner';
+import {GiftIcon, Instagram, Link} from 'lucide-react';
+import InstaLogo from '@/assets/instagram.png';
+import Chusuk2 from '@/assets/chusuk3.png';
+import {AspectRatio} from '@/components/ui/aspect-ratio';
 
 const GiftCard = React.lazy(() => import('./components/GiftCard'));
 const NotFound = React.lazy(() => import('../NotFound'));
 
 const Results = () => {
-    const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(true);
     const navigate = useNavigate();
     const {chatID} = useParams();
     const isLogin = useAtomValue(isLoginAtom);
@@ -80,15 +84,33 @@ const Results = () => {
                             </span>
                         </div>
                         {isValid && (
-                            <KakaoShare
-                                title={`ONE!T - ${userName === '' ? '' : userName + '을 위한 '}선물 추천`}
-                                description="WANNA GIFT IT, ONE!T"
-                                url={`https://www.oneit.gift/result/${chatID}`}
-                                image={product[0].thumbnailUrl}
-                            />
+                            // <KakaoShare
+                            //     title={`ONE!T - ${userName === '' ? '' : userName + '을 위한 '}선물 추천`}
+                            //     description="WANNA GIFT IT, ONE!T"
+                            //     url={`https://www.oneit.gift/result/${chatID}`}
+                            //     image={product[0].thumbnailUrl}
+                            // />
+                            <Button
+                                variant="ghost"
+                                size="icon"
+                                onClick={async () => {
+                                    await navigator.clipboard.writeText(
+                                        `https://oneit.gift/result/${chatID}`,
+                                    );
+                                    toast('클립보드에 복사되었습니다.');
+                                }}
+                            >
+                                <Link />
+                            </Button>
                         )}
 
-                        {/* <Share  url={`https://oneit.gift/result/${chatID}`} title={`ONE!T - ${userName===""?"":userName+"위한 "}선물 추천`} text={product.map(item => item.name).join('\n')}/> */}
+                        {/* <Share
+                            url={`https://oneit.gift/result/${chatID}`}
+                            title={`ONE!T - ${userName === '' ? '' : userName + '위한 '}선물 추천`}
+                            text={product
+                                .map((item) => item.name)
+                                .join('\n')}
+                        /> */}
                         {/* <Share2Icon/> */}
                     </CardTitle>
                     <div className="w-full">
@@ -186,6 +208,62 @@ const Results = () => {
                                     </Button>
                                 </div>
                             )}
+                            <div className="flex items-center">
+                                <Dialog defaultOpen={true}>
+                                    <DialogTrigger asChild>
+                                        <Button
+                                            variant="ghost"
+                                            className="animate-bounce fixed bottom-0 right-0 px-3 py-6 rounded-full shadow-lg m-1 bg-gradient-to-br from-oneit-blue to-oneit-pink text-oneit-gray hover:from-oneit-pink hover:to-oneit-blue"
+                                        >
+                                            <GiftIcon className="w-6 h-6" />
+                                        </Button>
+                                    </DialogTrigger>
+                                    <DialogContent className="flex flex-col items-center justify-center">
+                                        <DialogHeader>
+                                            <DialogTitle>
+                                                🌕 ONE!T 추석 이벤트 진행 중 🌕
+                                            </DialogTitle>
+                                            <DialogDescription className="text-oneit-gray">
+                                                <p>
+                                                    추천 결과 페이지를 캡쳐하여
+                                                    인스타그램에 공유하면 <br />
+                                                    추첨을 통해 총{' '}
+                                                    <span className="text-oneit-pink">
+                                                        10
+                                                    </span>{' '}
+                                                    분께 스타벅스 쿠폰을
+                                                    드립니다. ☕️
+                                                </p>
+                                            </DialogDescription>
+                                        </DialogHeader>
+                                        <div className="max-w-52 w-full">
+                                            <a
+                                                href="https://www.instagram.com/p/C_7LxRNpOMS"
+                                                target="_blank"
+                                                rel="noreferrer"
+                                            >
+                                                <AspectRatio
+                                                    className="justify-center flex"
+                                                    ratio={1 / 1}
+                                                >
+                                                    <img src={Chusuk2} />
+                                                </AspectRatio>
+                                            </a>
+                                        </div>
+
+                                        <a
+                                            href="https://www.instagram.com/p/C_7LxRNpOMS"
+                                            target="_blank"
+                                            rel="noreferrer"
+                                        >
+                                            <Button className="w-full">
+                                                <Instagram className="inline mr-2" />
+                                                인스타그램 바로가기
+                                            </Button>
+                                        </a>
+                                    </DialogContent>
+                                </Dialog>
+                            </div>
                         </div>
                     )}
                 </Card>
